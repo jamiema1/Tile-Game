569c5e6d4cd7900db59f48d22ffc832a93a5bd39
Jamie Ma <jkmm.1024@gmail.com>
2023-07-09 23:04:45 -0700

    Added running animation to model + jumping ability

--- Tile Game/Assets/Scripts/PlayerMovement.cs
+++ Tile Game/Assets/Scripts/PlayerMovement.cs
@@ -7,25 +7,51 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D playerRigidbody;
-    [SerializeField] float runSpeed = 5f;
+    Animator playerAnimator;
+    CapsuleCollider2D playerCollider;
 
-    void Start()
-    {
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 13f;
+
+    void Start() {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
     }
 
-    void Update()
-    {
+    void Update() {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
-        // Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value) {
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+            return;
+        }
+        
+        if (value.isPressed) {
+            playerRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
     }
 
     void Run() {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRigidbody.velocity.y);
         playerRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+        playerAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+        
+    }
+
+    void FlipSprite() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidbody.velocity.x), 1f);
+        }
     }
 }
